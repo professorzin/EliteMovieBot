124ba3151b00e01003f021e52cbf67ef8d59257d
professorzin <its.professorzin@gmail.com>
2026-06-06 13:42:44 +0530

    Update debug.py

--- plugins/debug.py
+++ plugins/debug.py
@@ -8,3 +8,7 @@ async def debug(client, message):
     print("UPDATE RECEIVED")
     print(message)
     print("DEBUG FILE VERSION 999")
+
+@Client.on_message()
+async def debug(client, message):
+    print("UPDATE RECEIVED:", message.text)
