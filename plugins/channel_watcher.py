67a82b188e9907dc4a5d2e89d97f2ce5eee9f8c5
professorzin <its.professorzin@gmail.com>
2026-06-06 14:08:05 +0530

    Update channel_watcher.py

--- plugins/channel_watcher.py
+++ plugins/channel_watcher.py
@@ -2,10 +2,10 @@ from pyrogram import Client, filters
 from services.indexer import index_file
 from config import CHANNEL_ID
 
-@Client.on_message(
-    filters.chat(CHANNEL_ID)
-    & filters.media
-)
+# @Client.on_message(
+#     filters.chat(CHANNEL_ID)
+#     & filters.media
+# )
 async def auto_index(client, m):
 
     await index_file(client, m)
